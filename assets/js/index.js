64fcc4d625e7c5bf8a285c5eb6d87faddb93dab5
acher249 <32597382+acher249@users.noreply.github.com>
2018-07-19 12:24:00 -0400

    update

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+
+    $("#answer1").on("click", function(event) {
+        console.log("Clicked Answer 1");
+    });
+    
+    $("#answer2").on("click", function(event) {
+        console.log("Clicked Answer 2");
+    });
+    
+    $("#answer3").on("click", function(event) {
+        console.log("Clicked Answer 3");
+    });
+    
+    $("#answer4").on("click", function(event) {
+        console.log("Clicked Answer 4");
+    });
+    
+});
