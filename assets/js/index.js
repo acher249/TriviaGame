bd96cc07612b36ec4303c6487c2dadaaef16ec22
Adam <jchernick2010@gmail.com>
2018-07-20 20:52:41 -0400

    update logic
    update logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,7 @@
 window.onload = function() {
     //on start
-    welcomeAlert();
+    stopwatch.welcomeAlert();
 
-    //******use these functions in stopwatch object to build game********
-    $("#stop").on("click", stopwatch.stop);
-    $("#reset").on("click", stopwatch.reset);
-    $("#start").on("click", stopwatch.start);
 };
 
 //  Variable that will hold our setInterval that runs the stopwatch
@@ -17,8 +13,6 @@ var stopwatch = {
 
     time: 0,
 
-    
-
     reset: function() {
 
         stopwatch.time = 0;
@@ -29,7 +23,6 @@ var stopwatch = {
 
     start: function() {
 
-        // DONE: Use setInterval to start the count here and set the clock to running.
         if (!clockRunning) {
         intervalId = setInterval(stopwatch.count, 1000);
         clockRunning = true;
@@ -40,7 +33,6 @@ var stopwatch = {
 
     stop: function() {
 
-        // DONE: Use clearInterval to stop the count here and set the clock to not be running.
         clearInterval(intervalId);
         clockRunning = false;
     },
@@ -59,7 +51,7 @@ var stopwatch = {
             console.log("times up");
             $(".timer").text("Time Remaining: " + "00:00");
 
-            nextQuestionAlert();
+            stopwatch.nextQuestionAlert();
         }
     },
 
@@ -80,27 +72,24 @@ var stopwatch = {
         }
 
         return minutes + ":" + seconds;
-    }
-};
+    },
 
-
-//Alert Functions ******
-
-function nextQuestionAlert() {    
-swal({
-    title: "Out of Time!",
-    icon: "error",
-    text: "Get Ready For The Next Question!",
-    button: "Next Question", 
-})
-.then(() => {
-    //Start The Game
-    stopwatch.start();
-    $(".timer").text("Time Remaining: " + "00:05"); //Override***
-    });
-}
-
-function welcomeAlert() {    
+    //Alert Functions ******
+    nextQuestionAlert: function() {    
+    swal({
+        title: "Out of Time!",
+        icon: "error",
+        text: "Get Ready For The Next Question!",
+        button: "Next Question", 
+    })
+    .then(() => {
+        //Start The Game
+        stopwatch.start();
+        $(".timer").text("Time Remaining: " + "00:05"); //Override***
+        });
+    },
+    
+    welcomeAlert: function() {    
     swal({
         title: "Get Ready For Trivia!",
         icon: "success",
@@ -111,4 +100,46 @@ function welcomeAlert() {
         //Start The Game
         stopwatch.start();
         });
-    };
+    }
+};
+
+// Logic for Questions
+
+//Put questions in Array
+//Put answers in Array
+
+var rainQuestionArray = ["What is the shape of rain drops?", 
+"What color is rain?", 
+"What is the rainiest city?", 
+"" , 
+"", ]
+
+// var correctAnswer = 
+
+
+
+// Get Weather to set which set of questions
+var APIKey = "166a433c57516f51dfab1f7edaed8413";
+
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=New York,US&units=imperial&appid=" + APIKey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+    .then(function(response) {
+
+    console.log(response);
+
+    // Transfer content to HTML
+    // $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    // $(".wind").text("Wind Speed: " + response.wind.speed);
+    // $(".humidity").text("Humidity: " + response.main.humidity);
+    // $(".temp").text("Temperature (F) " + response.main.temp);
+
+    // Log the data in the console as well
+    console.log("Wind Speed: " + response.wind.speed);
+    console.log("Humidity: " + response.main.humidity);
+    console.log("Temperature (F): " + response.main.temp);
+    });
